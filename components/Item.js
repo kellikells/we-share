92f3fe402738fe49294ab6f09a92f74cd4e0b256
kelli <kellikells@gmail.com>
2021-03-07 11:02:48 -0800

    made item disabled if quantity is 0

--- components/Item.js
+++ components/Item.js
@@ -2,26 +2,35 @@ import React, { useContext } from 'react';
 import { GlobalContext } from '../context/GlobalState';
 
 export const Item = ({ item }) => {
+
     const { deleteItem } = useContext(GlobalContext);
 
+
+
     // ternary operator to determine item color (have or don't have)
-    // const sign = item.itemQuantity <= 0 ? '-' : '+';
+    const sign = item.itemQuantity <= 0 ? 'negative' : 'positive';
 
 
     return (
-        <div className='list-item'>
 
-    
+        <div key={item.id} className={sign == 'positive' ? 'list-item' : 'list-item-disabled'}>
+
             <div className='list-grid'>
 
-                {/* quantity */}
+                
+                {/* ----- quantity ----- */}
                 <div className='item-qty'>{item.itemQuantity}</div>
 
-                {/* item name */}
-                <div className='item-name'>{item.itemName}</div>
+                
+                {/* ----- item name ----- */}
+                <div className={sign == 'positive' ? 'item-name' :
+                    'item-name-disabled'} >
+                    {item.itemName}
+                </div>
 
-                {/* buttons */}
-                <div className={item.itemQuantity <= 0 ? 'item-btn-group-negative' : 'item-btn-group-positive'} >
+                
+                {/* ----- buttons ----- */}
+                <div className={sign == 'positive' ? 'item-btn-group' : 'item-btn-group-disabled'} >
 
                     <button className='btn-use-one'>Use 1</button>
 
@@ -29,13 +38,13 @@ export const Item = ({ item }) => {
 
                 </div>
 
-                {/* trash button */}
+                
+                {/* ----- trash button ----- */}
                 <button onClick={() => deleteItem(item.id)} className='col-span-1  bg-white p-2 font-thin'>
                     <img src='/trash.svg' alt='delete item' />
                 </button>
 
             </div>
         </div>
-
     );
 }
