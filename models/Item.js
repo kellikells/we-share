67d5cd32df89bc46e32282dde62ba41607c868e7
kelli <kellikells@gmail.com>
2021-03-08 00:17:21 -0800

    setup db connection

---
+++ models/Item.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose');
+
+const ItemSchema = new mongoose.Schema({
+    itemName: {
+        type: String,
+        required: [true, 'Please add an item name'],
+        trim: true,
+        maxlength: [40, 'Item name cannot be more than 40 characters']
+    },
+    itemQuantity: {
+        type: Number,
+        required: true,
+        maxlength: [10, 'Limit quantity to 10 characters']
+    }
+});
+
+
+// if  the Item model aready exist, export that
+// if it doesn't,  create Item model with ItemSchema definition
+module.exports = mongoose.models.Item || mongoose.model('Item', ItemSchema);
---
+++ utils/dbConnect.js
@@ -0,0 +1,20 @@
+import mongoose from 'mongoose';
+
+const connection = {};
+
+// function to check if there is already a connection to the database, or else it will create a connection 
+async function dbConnect() {
+    if (connection.isConnected) {
+        return;
+    }
+
+    const db = await mongoose.connect(process.env.MONGODB_URI, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    });
+
+    connection.isConnected = db.connections[0].readyState;
+    console.log(connection.isConnected);
+}
+
+export default dbConnect;
