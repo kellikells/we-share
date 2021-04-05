47e7129f0667277cd3b94f2ecba4c877e83758f1
kelli <kellikells@gmail.com>
2021-04-04 19:27:12 -0700

    fixed mongoose error with useFindAndModify

--- utils/dbConnect.js
+++ utils/dbConnect.js
@@ -1,5 +1,5 @@
 import mongoose from 'mongoose';
-
+mongoose.set('useFindAndModify', false);
 const connection = {};
 
 // function to check if there is already a connection to the database, or else it will create a connection 
@@ -11,12 +11,13 @@ async function dbConnect() {
     const db = await mongoose.connect(process.env.MONGO_URI, {
         useNewUrlParser: true,
         useUnifiedTopology: true,
+        
     });
 
     connection.isConnected = db.connections[0].readyState;
 
     console.log('----------------');
-    console.log(connection.isConnected);
+    console.log(`connection.isConnected: ${connection.isConnected}`);
 }
 
 export default dbConnect;
