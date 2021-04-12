203d2469e0a32059311b7b05ed1f791677fdb571
kelli <kellikells@gmail.com>
2021-04-11 22:55:53 -0700

    password hash is working

--- pages/api/users/index.js
+++ pages/api/users/index.js
@@ -1,7 +1,7 @@
 import dbConnect from '../../../utils/dbConnect';   // database connection
 import User from '../../../models/User';            // User schema
 
-import bcrypt from 'bcrypt';                  // hashing and verifying password
+import bcrypt from 'bcryptjs';                  // hashing and verifying password
 import v4 from 'uuid';                        // create random user id
 import jwt from 'jsonwebtoken';               // allows creating a secure frontend session, and verified backend
 import assert from 'assert';                  // validator for the request body & required data on endpoints
@@ -25,17 +25,34 @@ export default async (req, res) => {
         case 'POST':
             try {
                 const userEmail = await User.findOne({ email: req.body.email });
-
-                console.log(`userEmail: ${userEmail}`);
-
-                // if the email is not already in the database 
+                
+                // proceed to create new user
                 if (userEmail == null) {
-                    const newUser = await User.create(req.body);
-                    res.status(201).json({
-                        success: true,
-                        data: newUser
+             
+
+                    const hashedpw = await bcrypt.genSalt(10, (err, salt) => {
+                        bcrypt.hash(req.body.password, salt, (err, hash) => {
+                            if (err) throw err;
+                            req.body.password = hash;
+                   
+                            const newUser = User.create(req.body);
+
+                        })
                     });
+
+
+                    // CHECK THIS !!!!!!!!!!! 
+                    // console.log(`hashedPW: ${req.body.password}`);
+
+
+
+                    // const newUser = await User.create(req.body);
+                    // res.status(201).json({
+                    //     success: true,
+                    //     data: newUser
+                    // });
                 }
+                // WARNING: EMAIL ALREADY IN DATABASE
                 else {
                     res.status(400).json({
                         success: false,
@@ -43,30 +60,14 @@ export default async (req, res) => {
                     })
                 }
 
-
-         
-
             } catch (err) {
-
-                // getting ERROR messages 
                 if (err.name === 'ValidationError') {
                     const messages = Object.values(err.errors).map(val => val.message);
-
-                    // const apiErrorKeys = Object.keys(err.errors);
-                    // const apiErrorMessages = Object.values(err.errors);
-
-                    // console.log(`***************`);
-                    // console.log('index: line 75- hit the IFFF');
-                    // console.log(apiErrorKeys);
-                    // console.log(apiErrorMessages);
-                    // console.log(err.errors);
-
-                    // 400: client error, 
-                    res.status(400).json({
+               
+                    res.status(400).json({     // 400: client error, 
                         success: false,
                         error: messages
                     });
-
                 } else {
                     console.log('index: line 87- hit the ELSE');
 
